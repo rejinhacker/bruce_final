51764b678fca85305b95b52287045575464ea926
rejinhacker <xtharejin@gmail.com>
2024-10-22 15:47:03 +1300

    Fixing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@supabase/supabase-js': './node_modules/@supabase/supabase-js/dist/index.mjs'
+    }
+  },
   build: {
     rollupOptions: {
       external: ['@supabase/supabase-js']
     }
   }
 });
-
