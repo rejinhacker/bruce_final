97400b800e453c5d852e3af10168206127d50edd
rejinhacker <xtharejin@gmail.com>
2024-10-22 15:43:08 +1300

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
-  build:{
-    rollupOptions:{
-      external:['@supabase/supbase-js']
+  build: {
+    rollupOptions: {
+      external: ['@supabase/supabase-js']
     }
   }
 });
+
